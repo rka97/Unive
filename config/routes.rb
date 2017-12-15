7502d09c7648451815048d184638996b793a58b2
rka <enrev91@gmail.com>
2017-12-15 17:53:17 +0200

    Fixed a bug with redirection.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
+  resources :users, :students, :teachers
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-  delete '/user', to: 'users#destroy'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get 'teachers/index'
   get 'students/index'
-  resources :users, :students, :teachers
+  delete '/user', to: 'users#destroy'
 end
