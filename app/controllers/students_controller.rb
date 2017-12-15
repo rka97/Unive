ddfacb1e69d2dcb3eaf7101d8e24326fc87d41bd
rka <enrev91@gmail.com>
2017-12-15 20:40:32 +0200

    Can now delete and edit users.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,36 @@
 class StudentsController < ApplicationController
     before_action :redirect_if_not_admin, only: [:index, :edit, :update, :destroy] 
 
+    def show
+
+    end
+
     def index
         @students = Student.paginate(page: params[:page])
     end
 
+    def edit
+        @student = Student.find(params[:id])
+    end
+
+    def update
+        @student = Student.find(params[:id])
+        if @student.update_attributes(student_params)
+          flash[:success] = "Student data updated"
+          redirect_to @student
+        else
+          render 'edit'
+        end
+      end
+    
     def destroy
         Student.find(params[:id]).destroy
         flash[:success] = "Student deleted"
         redirect_to students_url
+    end
+
+    private
+    def student_params
+        params.require(:student).permit(:name, :section_number, :bench_number, :gender, :year, :phone_number, :address, :birth_date)
     end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,5 @@ class Student < ApplicationRecord
     has_one :user, as: :profile_owner, dependent: :delete
     has_many :enrollments, dependent: :delete_all
     has_many :departments, through: :enrollments
+    validates_format_of :gender, :with => /\AMale\Z|\AFemale\Z|\ANA\Z/
 end
